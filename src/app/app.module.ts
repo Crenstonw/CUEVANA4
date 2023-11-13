548f3f1f4850691e08c3659a617b82e8edc948ba
Crenstonw <b2016rubensa@gmail.com>
2023-11-13 20:46:34 +0100

    errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { BarraTareasComponent } from './components/COMMON/barra-tareas/barra-tareas.component';
